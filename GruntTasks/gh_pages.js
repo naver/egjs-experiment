e918afb5dcbb5fc98273aa7dbf195f9105f0df42
Taihoon Kim <kishu@navercorp.com>
2016-01-15 15:03:52 +0900

    chore: remove bower.json. udpate gh_pages grunt task

--- GruntTasks/gh_pages.js
+++ GruntTasks/gh_pages.js
@@ -3,12 +3,11 @@ var path = require("path");
 
 module.exports = function(grunt) {
 	function config(components) {
-		var targets = ["bower_components/**/*", "egjs/**/*"];
-		targets =_.reduce(components, function(result, component) {
+		var targets =_.reduce(components, function(result, component) {
 			result.push(path.join(component, "src/**/*"));
 			result.push(path.join(component, "demo/**/*"));
 			return result;
-		}, targets);
+		}, []);
 
 		grunt.config("gh-pages", {
 			src: targets
